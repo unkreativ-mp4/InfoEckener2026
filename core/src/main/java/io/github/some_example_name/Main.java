73b93b84aff9cbcdd311d8bf71dc622523cfd2c2
Darkys1de <tristan-gruendemann@gmx.de>
2026-01-16 09:16:39 +0100

    Setup

---
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -0,0 +1,40 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input. */
+public class Main extends InputAdapter implements ApplicationListener {
+    @Override
+    public void create() {
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
+        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
+        if(width <= 0 || height <= 0) return;
+
+        // Resize your screen here. The parameters represent the new window size.
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    // Note: you can override methods from InputAdapter API to handle user's input.
+}
