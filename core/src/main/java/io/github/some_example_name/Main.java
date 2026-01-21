c12a774c2b75edfcde7c8e08dcedd3d730b59d63
tristan.gruendemann <tristan-gruendemann@gmx.de>
2026-01-21 14:09:29 +0100

    Diamantschwert Sprite hinzugefügt

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -3,16 +3,34 @@ package io.github.some_example_name;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input. */
 public class Main extends InputAdapter implements ApplicationListener {
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    Texture diamond_sword_texture;
+
     @Override
     public void create() {
         Gdx.input.setInputProcessor(this);
+
+        diamond_sword_texture = new Texture("Diamond_Sword_texture.png");
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
     }
 
     @Override
     public void resize(int width, int height) {
+
+        viewport.update(width, height, true);
         // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
         // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
         if(width <= 0 || height <= 0) return;
@@ -22,6 +40,8 @@ public class Main extends InputAdapter implements ApplicationListener {
 
     @Override
     public void render() {
+
+        draw();
     }
 
     @Override
@@ -34,6 +54,17 @@ public class Main extends InputAdapter implements ApplicationListener {
 
     @Override
     public void dispose() {
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        spriteBatch.draw(diamond_sword_texture, 0, 0, 1, 1);
+
+        spriteBatch.end();
     }
 
     // Note: you can override methods from InputAdapter API to handle user's input.
